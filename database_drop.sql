e62a2bc580b6806c01e2ecc66a2587fc29f5232f
Carlin Connell <carlin.connell@gmail.com>
2016-09-11 15:09:27 +1000

    renamed foreign keys

--- database_drop.sql
+++ database_drop.sql
@@ -1,41 +1,33 @@
-ALTER TABLE `Orders` DROP FOREIGN KEY `Orders_fk0`;
-
-ALTER TABLE `Orders` DROP FOREIGN KEY `Orders_fk1`;
-
-ALTER TABLE `Orders` DROP FOREIGN KEY `Orders_fk2`;
-
-ALTER TABLE `Orders` DROP FOREIGN KEY `Orders_fk3`;
-
-ALTER TABLE `Orders` DROP FOREIGN KEY `Orders_fk4`;
-
-ALTER TABLE `Orders` DROP FOREIGN KEY `Orders_fk5`;
-
-ALTER TABLE `Orders` DROP FOREIGN KEY `Orders_fk6`;
-
-ALTER TABLE `Website Administrators` DROP FOREIGN KEY `Website Administrators_fk0`;
-
-ALTER TABLE `Customers Pickup Addresses` DROP FOREIGN KEY `Customers Pickup Addresses_fk0`;
-
-ALTER TABLE `Customers Pickup Addresses` DROP FOREIGN KEY `Customers Pickup Addresses_fk1`;
-
-ALTER TABLE `Addresses` DROP FOREIGN KEY `Addresses_fk0`;
-
+ALTER TABLE `Orders` 
+	DROP FOREIGN KEY `Orders_fk_RECIPENT_ID`;
+ALTER TABLE `Orders` 
+	DROP FOREIGN KEY `Orders_fk_ORDER_PICKED_UP_BY`;
+ALTER TABLE `Orders` 
+	DROP FOREIGN KEY `Orders_fk_ORDER_DROPPED_OFF_BY`;
+ALTER TABLE `Orders` 
+	DROP FOREIGN KEY `Orders_fk_CUSTOMER_ID`;
+ALTER TABLE `Orders` 
+	DROP FOREIGN KEY `Orders_fk_STATUS_DESCRIPTION`;
+ALTER TABLE `Orders` 
+	DROP FOREIGN KEY `Orders_fk_PICKUP_LOCATION`;
+ALTER TABLE `Orders` 
+	DROP FOREIGN KEY `Orders_fk_DROP_OFF_LOCATION`;
+ALTER TABLE `Website Administrators` 
+	DROP FOREIGN KEY `Website Website_Administrators_fk_EMPLOYEE_ID`;
+ALTER TABLE `Customers Pickup Addresses` 
+	DROP FOREIGN KEY `Customers_Pickup_Addresses_ADDRESS_ID`;
+ALTER TABLE `Customers Pickup Addresses` 
+	DROP FOREIGN KEY `Customers_Pickup_Addresses_fk_CUSTOMER_ID`;
+ALTER TABLE `Addresses` 
+	DROP FOREIGN KEY `Addresses_fk_ADDRESS_POSTCODE`;
+	
 DROP TABLE IF EXISTS `Customers`;
-
 DROP TABLE IF EXISTS `Orders`;
-
 DROP TABLE IF EXISTS `Cost Weight`;
-
 DROP TABLE IF EXISTS `Employee`;
-
 DROP TABLE IF EXISTS `Status`;
-
 DROP TABLE IF EXISTS `Website Administrators`;
-
 DROP TABLE IF EXISTS `Customers Pickup Addresses`;
-
 DROP TABLE IF EXISTS `Addresses`;
-
 DROP TABLE IF EXISTS `Deliverable Postcodes`;
-
 DROP TABLE IF EXISTS `Recipent`;
