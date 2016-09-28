b8d81b8bbb62caeccf2dd44f140be2c951239cc2
group13kim <ksy4961@gmail.com>
2016-09-28 22:26:50 +1000

    Add files via upload

---
+++ login_page299.php
@@ -0,0 +1,47 @@
+
+
+	<?php
+		if(isset($_POST['submit']))
+		{
+			$CUSTOMER_ID = htmlentities($_POST['CUSTOMER_ID']);
+			$CUSTOMER_PASSWORD = htmlentities($_POST['CUSTOMER_PASSWORD']);
+			
+			$table = "logins";
+			
+			$con = mysqli_connect($hostname, $CUSTOMER_ID, $CUSTOMER_PASSWORD, $dbname) or die("Could not connect to database");
+			$query = "SELECT * FROM $table WHERE Customers ='$CUSTOMER_ID'";
+			
+			$query = mysqli_query($con, $query);
+			$result = mysqli_fetch_assoc($query);
+			if($result == NULL)
+			{
+				echo "<p class='unsuccessful'>Incorrect ID or password, please try again.</p>";
+
+			}
+
+		}
+		
+	}
+
+		
+		?>
+	<title>Login</title>
+	<h1>System Login</h1>
+		<p>Please enter your ID and password to continue.</p>
+		<form action="" method="POST" autocomplete="off">
+			<table>
+			<tr>
+			<td>ID: </td><td><input type="text" name="ID" placeholder="CUSTOMER_ID" required></td><td rowspan="3">
+				<img alt="Padlock" longdesc="Padlock" src="images/padlock.png" width="10%"></td>
+			</tr>
+			<tr>
+			<td>Password: </td><td><input type="password" name="password" placeholder="CUSTOMER_PASSWORD" required></td>
+			</tr>
+			<tr>
+			<td></td><td>
+				<input type="submit" name="submit" value="Login" /></td>
+			</tr>
+		</table>
+		</form>
+		<p><a href="signup299.php">I don't have an account</a></p>
+	
