c4d497bffb16ff3ce9cd9e4df2d18df248466c5f
Victor <1205138742@qq.com>
2016-10-07 14:26:13 +1000

    Update register299.php

--- register299.php
+++ register299.php
@@ -1,11 +1,11 @@
 <?php
-	          
-                		
+
+
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
 		$dbname = "ifb299";
-		
+
 		// Create connection
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
 		// Check connection
@@ -13,7 +13,7 @@
 		    die("Connection failed: " . mysqli_connect_error());
 		}
 
-		
+
 		//$link = mysqli_connect($servername, $username, $password, $dbname);
 		//if (!$link) {
 		//			echo "<p>Error: Unable to connect to MySQL." . PHP_EOL . "</p>";
@@ -24,90 +24,111 @@
 
        //$dbhandle = mysql_connect($hostname, $username, $password) or die("Could not connect to database");
        //$selecttable = mysql_select_db($database, $dbhandle);
-		
-		
-		
-		
+
+
+
+
      ?>
+     <?php
+         $validUser = 1;
+       if(isset($_POST['CUSTOMER_ID']) && isset($_POST['CUSTOMER_FIRST_NAME']) && isset($_POST['CUSTOMER_LAST_NAME']) && isset($_POST['CUSTOMER_USERNAME']) && isset($_POST['CUSTOMER_PASSWORD']) && isset($_POST['CUSTOMER_PASSWORD1']) && isset($_POST['CUSTOMER_MOBILE']))
+       {
+         $CUSTOMER_ID = $_POST['CUSTOMER_ID'];
+         $CUSTOMER_FIRST_NAME = $_POST['CUSTOMER_FIRST_NAME'];
+         $CUSTOMER_LAST_NAME = $_POST['CUSTOMER_LAST_NAME'];
+         $CUSTOMER_USERNAME = $_POST['CUSTOMER_USERNAME'];
+         $CUSTOMER_PASSWORD = $_POST['CUSTOMER_PASSWORD'];
+         $CUSTOMER_PASSWORD1 = $_POST['CUSTOMER_PASSWORD1'];
+         $CUSTOMER_MOBILE = $_POST['CUSTOMER_MOBILE'];
+         $CUSTOMER_HOMEPHONE = $_POST['CUSTOMER_HOMEPHONE'];
 
-<title>Signup</title>
+         if ($CUSTOMER_PASSWORD != $CUSTOMER_PASSWORD1) {
+           echo '<p class="unsuccessful">Passwords did not match please try again!</p>';
+           $validUser = 0;
+         }
+
+         $sql = "SELECT * FROM customers WHERE CUSTOMER_ID ='$CUSTOMER_ID'";
+         $result = mysqli_query($conn, $sql);
+
+         if(mysqli_num_rows($result) > 0)
+         {
+           echo '<p class="unsuccessful">CUSTOMER_ID already taken. Please try again.</p>';
+           $validUser = 0;
+         }
+         else
+         {
+           $validUser = 1;
+           // $CUSTOMER_PASSWORD = password_hash($CUSTOMER_PASSWORD, PASSWORD_DEFAULT);
+
+           //$sql = "INSERT INTO customers(CUSTOMER_ID, CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CUSTOMER_USERNAME, CUSTOMER_PASSWORD, CUSTOMER_PASSWORD1, CUSTOMER_MOBILE) VALUES ('$CUSTOMER_ID', '$CUSTOMER_FIRST_NAME', '$CUSTOMER_LAST_NAME', '$CUSTOMER_USERNAME','$CUSTOMER_PASSWORD`, `$CUSTOMER_PASSWORD1`, `$CUSTOMER_MOBILE')";
+           $sql = "INSERT INTO customers(CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CUSTOMER_USERNAME, CUSTOMER_PASSWORD, CUSTOMER_MOBILE, CUSTOMER_HOMEPHONE) VALUES ('$CUSTOMER_FIRST_NAME','$CUSTOMER_LAST_NAME', '$CUSTOMER_USERNAME', '$CUSTOMER_PASSWORD', '$CUSTOMER_MOBILE', '$CUSTOMER_HOMEPHONE')";
+
+           $result = mysqli_query($conn, $sql);
+         }
+       }
+
+       mysqli_close($conn);
+
+        ?>
 
 
-	<h1>New Customer Signup</h1>
-	<p>Please complete this form to signup for a new account. All fields must be 
-	completed.</p>
-	<?php
-			$validUser = 1;
-		if(isset($_POST['CUSTOMER_ID']) && isset($_POST['CUSTOMER_FIRST_NAME']) && isset($_POST['CUSTOMER_LAST_NAME']) && isset($_POST['CUSTOMER_USERNAME']) && isset($_POST['CUSTOMER_PASSWORD']) && isset($_POST['CUSTOMER_PASSWORD1']) && isset($_POST['CUSTOMER_MOBILE']))
-		{
-			$CUSTOMER_ID = $_POST['CUSTOMER_ID'];
-			$CUSTOMER_FIRST_NAME = $_POST['CUSTOMER_FIRST_NAME'];
-			$CUSTOMER_LAST_NAME = $_POST['CUSTOMER_LAST_NAME'];
-			$CUSTOMER_USERNAME = $_POST['CUSTOMER_USERNAME'];
-			$CUSTOMER_PASSWORD = $_POST['CUSTOMER_PASSWORD'];
-			$CUSTOMER_PASSWORD1 = $_POST['CUSTOMER_PASSWORD1'];
-			$CUSTOMER_MOBILE = $_POST['CUSTOMER_MOBILE'];
-			$CUSTOMER_HOMEPHONE = $_POST['CUSTOMER_HOMEPHONE'];
-			
-			if ($CUSTOMER_PASSWORD != $CUSTOMER_PASSWORD1) {
-				echo '<p class="unsuccessful">Passwords did not match please try again!</p>';
-				$validUser = 0;
-			}
-			
-			$sql = "SELECT * FROM customers WHERE CUSTOMER_ID ='$CUSTOMER_ID'";
-			$result = mysqli_query($conn, $sql);
 
-			if(mysqli_num_rows($result) > 0)
-			{
-				echo '<p class="unsuccessful">CUSTOMER_ID already taken. Please try again.</p>';
-				$validUser = 0;
-			}
-			else
-			{
-				$validUser = 1;
-				// $CUSTOMER_PASSWORD = password_hash($CUSTOMER_PASSWORD, PASSWORD_DEFAULT);
-	 			
-	 			//$sql = "INSERT INTO customers(CUSTOMER_ID, CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CUSTOMER_USERNAME, CUSTOMER_PASSWORD, CUSTOMER_PASSWORD1, CUSTOMER_MOBILE) VALUES ('$CUSTOMER_ID', '$CUSTOMER_FIRST_NAME', '$CUSTOMER_LAST_NAME', '$CUSTOMER_USERNAME','$CUSTOMER_PASSWORD`, `$CUSTOMER_PASSWORD1`, `$CUSTOMER_MOBILE')";
-	 			$sql = "INSERT INTO customers(CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CUSTOMER_USERNAME, CUSTOMER_PASSWORD, CUSTOMER_MOBILE, CUSTOMER_HOMEPHONE) VALUES ('$CUSTOMER_FIRST_NAME','$CUSTOMER_LAST_NAME', '$CUSTOMER_USERNAME', '$CUSTOMER_PASSWORD', '$CUSTOMER_MOBILE', '$CUSTOMER_HOMEPHONE')";
-	 			
-				$result = mysqli_query($conn, $sql);
-			}
-		}
 
-		mysqli_close($conn);
-		
-     ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>On the Spot - Your Package Delivery Needs!</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css"></link>
+</head>
+<body>
+  <div id="nav"><br>
+      <div id="nav_wrapper"><br>
+          <ul>
+              <li><a href="home.html">Home</a></li>
+              <li><a href="order.html">Order</a></li>
+              <li><a href="tracking.html">Tracking</a></li>
+              <li><a href="contact & locations.html">Contact & Locations</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="help.html">Help</a></li>
+              <li><a href="login.php">Login</a></li>
+              <li><a href="loginstaff.php">Staff Login</a></li>
+          </ul>
+        </div>
+    </div>
+</body>
+<body>
+    <div class="container">
+      <img src="img/loginmen.png">
+          <h1>New Customer Signup</h1>
 
-	<form action="signup299.php" method="POST" autocomplete="off">
-		<table>
-		<tr>
-		<td>ID:</td><td><input type="text" name="CUSTOMER_ID" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_ID.'"'; }?> required/></td>
-		</tr>
-		<tr>
-		<td>First Name:</td><td><input type="text" name="CUSTOMER_FIRST_NAME" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_FIRST_NAME.'"'; }?> required/></td>
-		</tr>
-		<tr>		
-		<td>Last Name:</td><td><input type="text" name="CUSTOMER_LAST_NAME" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_LAST_NAME.'"'; }?> required/></td>
-		</tr>
-		<tr>		
-		<td>User Name:</td><td><input type="text" name="CUSTOMER_USERNAME" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_USERNAME.'"'; }?> required/></td>
-		</tr>
+          <form action="signup299.php" method="POST" autocomplete="off">
+            <table>
+              <tr>
+                <td><input type="text" name="CUSTOMER_FIRST_NAME" placeholder="Enter First Name*"<?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_FIRST_NAME.'"'; }?> required/></td>
+              </tr>
+              <tr>
+                <td><input type="text" name="CUSTOMER_LAST_NAME" placeholder="Enter Last Name*"<?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_LAST_NAME.'"'; }?> required/></td>
+              </tr>
+              <tr>
+                <td><input type="text" name="CUSTOMER_USERNAME" placeholder="Enter User Name*"<?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_USERNAME.'"'; }?> required/></td>
+              </tr>
+              <tr>
+                <td><input type="password" name="CUSTOMER_PASSWORD" required /placeholder="Create Password*"></td>
+              </tr>
+              <tr>
+                <td><input type="password" name="CUSTOMER_PASSWORD1" required /placeholder="Enter Password Again*"></td>
+              </tr>
+              <tr>
+                <td><input type="text" name="CUSTOMER_MOBILE" placeholder="Enter Movile phone*"<?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_MOBILE.'"'; }?> required/></td>
+              </tr>
+              <tr>
+                <td><input type="submit" name="Submit" value="Submit" class="btn-login"></td>
+              </tr>
 
-		<tr>
-		<td>Password:</td><td><input type="password" name="CUSTOMER_PASSWORD" required /></td>
-		</tr>
-		<tr>
-		<td>Repeat Password:</td><td><input type="password" name="CUSTOMER_PASSWORD1" required /></td>
-		</tr>
-		<tr>
-		<td>Phone Number:</td><td><input type="text" name="CUSTOMER_MOBILE" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_MOBILE.'"'; }?> required/></td>
-		</tr>
-		<tr>
-		<td>Phone Number:</td><td><input type="text" name="CUSTOMER_HOMEPHONE" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_HOMEPHONE.'"'; }?> required/></td>
-		</tr>
-		<tr>		
-		
-		<td></td><td><input type="submit" value="Submit" name="Submit" /></td>
-	</tr>
-	</table>
-	</form>
+            </table>
+          </form>
+          <p>*Please complete this form to signup for a new account. All fields must be
+        	completed.</p>
+    </div>
+</body>
+</html>
