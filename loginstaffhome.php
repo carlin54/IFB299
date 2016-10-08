06acac8686281785b53f588cd6ca3cdbf466ff85
Victor <1205138742@qq.com>
2016-10-08 18:13:09 +1000

    Update loginstaffhome.php

--- loginstaffhome.php
+++ loginstaffhome.php
@@ -6,22 +6,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>On the Spot - Your Package Delivery Needs!</title>
+    <!---<h1>On the Spot - Your Package Delivery Needs!</h1>--->
     <link rel="stylesheet" type="text/css" href="loginhomestyle.css"></link>
 </head>
-<div class="header">
+<body>
+  <div class="header">
     <h1>On the Spot - Your Package Delivery Needs!</h1>
-</div>
-<div>
-  <h1>
-  Welcome Staff<br>
-  <?php echo $_SESSION['EMPLOYEE_USERNAME']; ?>
-  </h1>
-</div>
+  </div>
+  <div id="nav"><br>
+    <div id="nav_wrapper"><br>
+      <ul>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="order.html">Order</a></li>
+        <li><a href="tracking.html">Tracking</a></li>
+        <li><a href="contact & locations.html">Contact & Locations</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="help.html">Help</a></li>
+        <li><a href="stafflogout.php">Staff Lougout</a></li>
+      </ul>
+    </div>
+  </div>
+</body>
 
-<div>
-    <a href="stafflogout.php">Lougout</a>
-</div>
-
+<h1>Welcome, <?php echo $_SESSION['EMPLOYEE_USERNAME']; ?></h1>
+<h2>Your Task Information</h2>
+<body>
+  <div class="container">
+    <h1>
+      Task One......<br>
+      Task Two......<br>
+      Task Three......<br>
+      Task Four......<br>
+    </h1>
+  </div>
 </body>
 </html>
