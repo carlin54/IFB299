c82998a88b7e054b19fdf0cf3fc54b5d8bfae93b
Victor <1205138742@qq.com>
2016-10-13 13:52:00 +1000

    Update register299.php

--- register299.php
+++ register299.php
@@ -1,11 +1,11 @@
 <?php
-	          
-                		
+
+
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
 		$dbname = "ifb299";
-		
+
 		// Create connection
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
 		// Check connection
@@ -14,8 +14,8 @@
 		}
 
 
-		
-		
+
+
      ?>
 
 <title>Signup</title>
@@ -32,12 +32,12 @@
 			$CUSTOMER_PASSWORD1 = $_POST['CUSTOMER_PASSWORD1'];
 			$CUSTOMER_MOBILE = $_POST['CUSTOMER_MOBILE'];
 			$CUSTOMER_HOMEPHONE = $_POST['CUSTOMER_HOMEPHONE'];
-			
+
 			if ($CUSTOMER_PASSWORD != $CUSTOMER_PASSWORD1) {
 				echo '<p class="unsuccessful">Passwords did not match please try again!</p>';
 				$validUser = 0;
 			}
-			
+
 			$sql = "SELECT * FROM customers WHERE CUSTOMER_USERNAME ='$CUSTOMER_USERNAME'";
 			$result = mysqli_query($conn, $sql);
 
@@ -50,14 +50,13 @@
 			{
 				$validUser = 1;
 				$sql = "INSERT INTO customers(CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, CUSTOMER_USERNAME, CUSTOMER_PASSWORD, CUSTOMER_MOBILE, CUSTOMER_HOMEPHONE) VALUES ('$CUSTOMER_FIRST_NAME','$CUSTOMER_LAST_NAME', '$CUSTOMER_USERNAME', '$CUSTOMER_PASSWORD', '$CUSTOMER_MOBILE', '$CUSTOMER_HOMEPHONE')";
-	 			
+
 				$result = mysqli_query($conn, $sql);
-				header("location: login.php");
 			}
 		}
 
 		mysqli_close($conn);
-		
+
      ?>
 
 <!DOCTYPE html>
@@ -70,7 +69,7 @@
   <div id="nav"><br>
       <div id="nav_wrapper"><br>
           <ul>
-              <li><a href="home.html">Home</a></li>
+              <li><a href="home.php">Home</a></li>
               <li><a href="order.html">Order</a></li>
               <li><a href="tracking.html">Tracking</a></li>
               <li><a href="contact & about.html">Contact & About</a></li>
@@ -85,16 +84,16 @@
     <div class="container">
       <img src="img/register.png">
       <h1>New Customer Signup</h1>
-	
+
 	<form action="register.php" method="POST" autocomplete="off">
-		<table>
+		<!--<table>
 		<tr>
 		<td>First Name:</td><td><input type="text" name="CUSTOMER_FIRST_NAME" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_FIRST_NAME.'"'; }?> required/></td>
 		</tr>
-		<tr>		
+		<tr>
 		<td>Last Name:</td><td><input type="text" name="CUSTOMER_LAST_NAME" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_LAST_NAME.'"'; }?> required/></td>
 		</tr>
-		<tr>		
+		<tr>
 		<td>User Name:</td><td><input type="text" name="CUSTOMER_USERNAME" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_USERNAME.'"'; }?> required/></td>
 		</tr>
 
@@ -110,13 +109,44 @@
 		<tr>
 		<td>Phone Number:</td><td><input type="text" name="CUSTOMER_HOMEPHONE" <?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_HOMEPHONE.'"'; }?> required/></td>
 		</tr>
-		<tr>		
+		<tr>
 		 <td></td><td><input type="submit" value="Submit" name="Submit" class="btn-register" /></td>
 	</tr>
+</table>-->
+
+	<table>
+		<tr>
+			<td><input type="text" name="CUSTOMER_FIRST_NAME" placeholder="Enter First Name*"<?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_FIRST_NAME.'"'; }?> required/></td>
+		</tr>
+		<tr>
+			<td><input type="text" name="CUSTOMER_LAST_NAME" placeholder="Enter Last Name*"<?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_LAST_NAME.'"'; }?> required/></td>
+		</tr>
+		<tr>
+			<td><input type="text" name="CUSTOMER_USERNAME" placeholder="Enter User Name*"<?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_USERNAME.'"'; }?> required/></td>
+		</tr>
+		<tr>
+			<td><input type="password" name="CUSTOMER_PASSWORD" required /placeholder="Create Password*"></td>
+		</tr>
+		<tr>
+			<td><input type="password" name="CUSTOMER_PASSWORD1" required /placeholder="Enter Password Again*"></td>
+		</tr>
+		<tr>
+			<td><input type="text" name="CUSTOMER_MOBILE" placeholder="Enter Movile phone*"<?php if ($validUser == 0){ echo 'value="'.$CUSTOMER_MOBILE.'"'; }?> required/></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="Submit" value="Submit" class="btn-register"></td>
+		</tr>
+
 	</table>
+
+
+
+
+
 	</form>
+	<div>
 		<p>*Please complete this form to signup for a new account. All fields must be	completed.</p>
-						Already have an account? <a href = "login.php" tite = "login">Login now!
-						</div>
+		Already have an account? <a href = "login.php" tite = "login">Login now!
+	</div>
 </body>
 </html>
